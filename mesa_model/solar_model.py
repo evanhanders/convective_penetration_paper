e85813ebdd9aa55fcda3ad1eb4872e3727a46456
Evan Anders <evanhanders@gmail.com>
2021-04-16 16:59:58 -0500

    updates to mesa profile plotting from adam

--- mesa_model/solar_model.py
+++ mesa_model/solar_model.py
@@ -33,41 +33,42 @@ mass, r = mass.cgs, r.cgs
 rho = 10**p.logRho[::-1] * u.g / u.cm**3
 P = 10**p.logP[::-1] * u.g / u.cm / u.s**2
 T = 10**p.logT[::-1] * u.K
+g = p.grav[::-1] * (u.cm / u.s**2)
 opacity = p.opacity[::-1] * (u.cm**2 / u.g)
 Luminosity = p.luminosity[::-1] * u.L_sun
 Luminosity = Luminosity.cgs
 mlt_conv_vel = p.conv_vel * u.cm / u.s
-g = constants.G.cgs*mass/r**2
-
 conv_lum = p.conv_L_div_L[::-1] * Luminosity
+rad_lum = 10**(p.log_Lrad[::-1])* u.L_sun
+rad_lum = rad_lum.cgs
 
-grad_rad = p.gradr[::-1]
+
+#Get the gradients => grad = dlnT/dlnP
+#grad = dlnT/dlnP = (dlnT/dr)*(dr/dlnP) = (P/T)*(dT/dr)*(dr/dP)
+grad_rad = 3*opacity*P*Luminosity / (16 * constants.sigma_sb.cgs * g * T**4 * 4 * np.pi * r**2)
+grad_actual = p.gradT[::-1]
 grad_ad  = (p.gradr_div_grada[::-1])**(-1) * grad_rad
 
-
+#Radiative conductivity
 rad_cond = 16 * constants.sigma_sb.cgs * T**3 / (3 * rho * opacity) # = rho * cp * rad_diffusivity
 rad_cond = rad_cond.cgs
 
+#Calculate temperature gradients
+grad_T_rad = -(T/P)*rho*g*grad_rad
+grad_T_ad  = -(T/P)*rho*g*grad_ad
+grad_T_actual  = -(T/P)*rho*g*grad_actual
 
-
-#grad = dlnT/dlnP = (dlnT/dr)*(dr/dlnP) = (P/T)*(dT/dr)*(dr/dP)
-my_grad_rad = 3*opacity*P*Luminosity / (16 * constants.sigma_sb.cgs * g * T**4 * 4 * np.pi * r**2)
-
-
+#Find bottom of CZ
 bot_cz = sop.brentq(interp1d(r, grad_rad - grad_ad), (0.6*u.R_sun).cgs.value, (0.8*u.R_sun).cgs.value)
 bot_cz_Rsol = sop.brentq(interp1d(r_Rsol, grad_rad - grad_ad), 0.6, 0.8)
 
-
+#Set up plot boundaries
 bot_plot = bot_cz - (0.1*u.R_sun).cgs.value
 top_plot = bot_cz + (0.1*u.R_sun).cgs.value
 good = (r.value >= bot_plot)*(r.value <= top_plot)
 
 bot_plot_Rsol = bot_cz_Rsol - 0.1
 top_plot_Rsol = bot_cz_Rsol + 0.1
-
-
-grad_T_rad = -(T/P)*rho*g*grad_rad
-grad_T_ad  = -(T/P)*rho*g*grad_ad
 
 rho_interface = interp1d(r, rho)(bot_cz)
 R_interface = P/(rho_interface*T)
@@ -82,7 +83,6 @@ bouss_grad_T_ad  = -(T/P)*rho_interface*g*grad_ad
 plt.figure()
 plt.axvline(bot_cz_Rsol, c='k', lw=0.5)
 plt.plot(r_Rsol, grad_rad, c='r', label=r'$\nabla_{\rm{rad}}$')
-#plt.plot(r_Rsol, my_grad_rad, ls='--', c='k')
 plt.plot(r_Rsol, grad_ad, c='b', label=r'$\nabla_{\rm{ad}}$')
 plt.ylim(0, 3)
 plt.xlim(bot_plot_Rsol, top_plot_Rsol)
@@ -95,8 +95,6 @@ avg_mag_diff = np.sum((np.abs((grad_T_rad - grad_T_ad)*r**2*np.gradient(r)))[goo
 plt.axvline(bot_cz_Rsol, c='k', lw=0.5)
 plt.plot(r_Rsol, -grad_T_rad, label=r'$\frac{\partial T_{\rm{rad}}}{\partial z}$', c='r')
 plt.plot(r_Rsol, -grad_T_ad, label=r'$\frac{\partial T_{\rm{ad}}}{\partial z}$', c='b')
-#plt.plot(r_Rsol, -bouss_grad_T_rad, label=r'$\frac{\partial T_{\rm{rad}}}{\partial z}$, $\rho = \rho_{\rm{interface}}$', c='r', ls='--')
-#plt.plot(r_Rsol, -bouss_grad_T_ad, label=r'$\frac{\partial T_{\rm{ad}}}{\partial z}$, $\rho = \rho_{\rm{interface}}$', c='b', ls='--')
 plt.yscale('log')
 plt.xlim(bot_plot_Rsol, top_plot_Rsol)
 plt.legend()
@@ -122,7 +120,9 @@ L_ad = -4*np.pi*r**2 * rad_cond * grad_T_ad
 plt.axvline(bot_cz_Rsol, c='k', lw=0.5)
 plt.plot(r_Rsol, Luminosity, c='orange', label=r'$L_{\odot}$')
 plt.plot(r_Rsol, conv_lum, c='k', label=r'$L_{\rm{conv}}$')
-plt.plot(r_Rsol, L_ad, c='indigo', label=r'$L_{\rm{cond, ad}} = -4\pi r^2 k_{\rm{rad}} \frac{\partial T_{\rm{ad}}}{\partial z}$')
+plt.plot(r_Rsol, rad_lum, c='red', label=r'$L_{\rm{rad}}$')
+plt.plot(r_Rsol, conv_lum + rad_lum, c='k', ls='--', label=r'$L_{\rm{conv}} + L_{\rm{rad}}$')
+plt.plot(r_Rsol, L_ad, c='indigo', ls='--', label=r'$L_{\rm{cond, ad}} = -4\pi r^2 k_{\rm{rad}} \frac{\partial T_{\rm{ad}}}{\partial z}$')
 plt.xlim(bot_plot_Rsol, top_plot_Rsol)
 plt.ylim(0, 2*Luminosity.max().value)
 plt.legend()
