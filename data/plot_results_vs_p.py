6ffe4bd6b918432dfd294f63750a75f5013bdef0
Evan Anders <evanhanders@gmail.com>
2021-07-23 14:17:57 -0500

    all parameter space plots now plot out of .csv files

--- data/plot_results_vs_p.py
+++ data/plot_results_vs_p.py
@@ -4,63 +4,50 @@ import matplotlib.pyplot as plt
 plt.style.use('apj')
 import h5py
 
-from matplotlib.patches import ConnectionPatch
-
 col_width = 3.25
 page_width = 6.5
 golden_ratio = 1.61803398875
 
-dirs = glob.glob('noslip_linear_P_cut/linear*')
-dirs += glob.glob('noslip_erf_P_cut/erf*')
 
-data = []
-for d in dirs:
-    S = float(d.split('_S')[-1].split('_')[0])
-    P = float(d.split('_Pr')[0].split('_P')[-1].split('_')[0])
-    Re = float(d.split('_Re')[-1].split('_')[0])
-    Lz = float(d.split('_Lz')[-1].split('_')[0])
-    with h5py.File('{:s}/data_top_cz.h5'.format(d), 'r') as f:
-        times = f['times'][()]
-        L_d01s = f['L_d01s'][()]
-        L_d05s = f['L_d05s'][()]
-        L_d09s = f['L_d09s'][()]
-        modern_f = f['modern_f'][()]
-        modern_xi = f['modern_xi'][()]
-        fconv_Ls = f['fconv_Ls'][()]
-        vel_rms = f['vel_cz'][()]
-        Ls = np.mean(f['Ls'][()])
-        tot_time = times[-1] - times[0]
-        time_window = np.min((500, tot_time/2))
-        good_times = times > (times[-1] - time_window)
-        mean_L_d01 = np.mean(L_d01s[good_times])
-        mean_L_d05 = np.mean(L_d05s[good_times])
-        mean_L_d09 = np.mean(L_d09s[good_times])
-        mean_f  = np.mean(modern_f[good_times])
-        mean_xi = np.mean(modern_xi[good_times])
-        mean_fconv_Ls = np.mean(fconv_Ls[good_times])
-        mean_vel_rms = np.mean(vel_rms[good_times])
-    if 'erf' in d:
-        erf = True
-    else:
-        erf = False
-
-    data.append((S, P, Re, Ls, Lz, erf, mean_L_d01, mean_L_d05, mean_L_d09, mean_f, mean_xi, mean_fconv_Ls, mean_vel_rms))
-data = np.array(data)
-
-S = data[:,0]
-P = data[:,1]
-Re = data[:,2]
-Ls = data[:,3]
-Lz = data[:,4]
+data = np.genfromtxt('scalar_table_data.csv', skip_header=1, delimiter=',', dtype=np.unicode_)
+S  = np.array(data[:,0], dtype=np.float64)
+P  = np.array(data[:,1], dtype=np.float64)
+Re = np.array(data[:,2], dtype=np.float64)
+Lz = np.array(data[:,3], dtype=np.float64)
 erf = data[:,5]
+sf  = data[:,6]
+ae  = data[:,7]
+d01 = np.array(data[:,10], dtype=np.float64)
+d05 = np.array(data[:,11], dtype=np.float64)
+d09 = np.array(data[:,12], dtype=np.float64)
+f   = np.array(data[:,13], dtype=np.float64)
+xi  = np.array(data[:,14], dtype=np.float64)
 
-L_d01 = data[:,6]
-L_d05 = data[:,7]
-L_d09 = data[:,8]
-theory_f  = data[:,9]
-theory_xi = data[:,10]
-fconv_Ls = data[:,11]
-vel_rms  = data[:,12]
+tmp = np.zeros_like(erf, dtype=bool)
+for i in range(erf.shape[0]):
+    if 'False' in erf[i]:
+        tmp[i] = 0
+    elif 'True' in erf[i]:
+        tmp[i] = 1
+erf = np.copy(tmp)
+
+tmp = np.zeros_like(sf, dtype=bool)
+for i in range(sf.shape[0]):
+    if 'False' in sf[i]:
+        tmp[i] = 0
+    elif 'True' in sf[i]:
+        tmp[i] = 1
+sf = np.copy(tmp)
+
+tmp = np.zeros_like(ae, dtype=bool)
+for i in range(ae.shape[0]):
+    if 'False' in ae[i]:
+        tmp[i] = 0
+    elif 'True' in ae[i]:
+        tmp[i] = 1
+ae = np.copy(tmp)
+
+
 
 fig = plt.figure(figsize=(page_width, 3*page_width/(3*golden_ratio)))
 
@@ -71,22 +58,16 @@ ax1_2 = fig.add_axes([0.00, 0.00, 0.46, 0.3])
 ax2_1 = fig.add_axes([0.54, 0.30, 0.46, 0.3])
 ax2_2 = fig.add_axes([0.54, 0.00, 0.46, 0.3])
 
-good = (erf == 1) * (Re == 4e2) * (S == 1e3) 
+good = (erf == 1) * (ae == 1) * (sf == 0) * (Re == 4e2) * (S == 1e3) 
 #Lcz_erf = (Ls*(fconv_Ls/(0.2*Ls)))[good]
-ax1.scatter(P[good], (L_d09[good] - Ls[good]), c='k', label=r"$\delta_{0.9}$", zorder=1, marker='v')
-ax1.scatter(P[good], (L_d05[good] - Ls[good]), c='k', label=r"$\delta_{0.5}$", zorder=1, marker='o')
-ax1.scatter(P[good], (L_d01[good] - Ls[good]), c='k', label=r"$\delta_{0.1}$", zorder=1, marker='^')
-ax1_1.scatter(P[good], theory_f[good], c='k')
-ax1_2.scatter(P[good], theory_xi[good], c='k')
+ax1.scatter(P[good], d09[good], c='k', label=r"$\delta_{0.9}$", zorder=1, marker='v')
+ax1.scatter(P[good], d05[good], c='k', label=r"$\delta_{0.5}$", zorder=1, marker='o')
+ax1.scatter(P[good], d01[good], c='k', label=r"$\delta_{0.1}$", zorder=1, marker='^')
+ax1_1.scatter(P[good], f[good], c='k')
+ax1_2.scatter(P[good], xi[good], c='k')
 leg1 = ax1.legend(frameon=True, fontsize=10, framealpha=0.6, loc='lower right')
 
 line_P = np.linspace(0, 21, 100)
-#line_f = 0.65
-#line_xi = 0.55
-#line_theory =  line_P * (1 - line_f) / (1 + line_P*line_f*line_xi)
-#theory_plot = ax1.plot(line_P, line_theory, c='orange', label=r'theory ($f = {{{:.2f}}}, \xi = {{{:.2f}}}$)'.format(line_f, line_xi), zorder=0)
-#ax1_1.axhline(line_f, c='orange', lw=0.5, zorder=0)
-#ax1_2.axhline(line_xi, c='orange', lw=0.5, zorder=0)
 line_theory = 0.13 * line_P
 theory_plot = ax1.plot(line_P, line_theory, c='orange', label=r'$\propto \mathcal{P}_D$', zorder=0)
 
@@ -100,30 +81,20 @@ ax1.set_yticks((0.25, 0.5, 0.75, 1, 1.25))
 
 
 leg2 = ax1.legend(frameon=True, fontsize=10, framealpha=0.6, loc='upper left')
-#leg2 = ax1.legend(theory_plot,[r'theory ($f = {{{:.2f}}}, \xi = {{{:.2f}}}$)'.format(line_f, line_xi)],  frameon=True, fontsize=10, framealpha=0.6, loc='upper left')
 leg2 = ax1.legend(theory_plot,[r'$\propto \mathcal{P}_D$'],  frameon=True, fontsize=10, framealpha=0.6, loc='upper left')
 ax1.add_artist(leg1)
 
 
 #Linear runs
-good = (erf == 0) * (Re == 8e2) * (S == 1e3) 
-#Lcz_linear = (Ls*(fconv_Ls/0.1)**(1/2))[good]
-ax2.scatter(P[good], (L_d09[good] - Ls[good]), c='k', zorder=1, marker='v')
-ax2.scatter(P[good], (L_d05[good] - Ls[good]), c='k', zorder=1, marker='o')
-ax2.scatter(P[good], (L_d01[good] - Ls[good]), c='k', zorder=1, marker='^')
-ax2_1.scatter(P[good], theory_f[good], c='k')
-ax2_2.scatter(P[good], theory_xi[good], c='k')
+good = (erf == 0) * (ae == 1) * (sf == 0) * (Re == 8e2) * (S == 1e3) 
+ax2.scatter(P[good], d09[good], c='k', zorder=1, marker='v')
+ax2.scatter(P[good], d05[good], c='k', zorder=1, marker='o')
+ax2.scatter(P[good], d01[good], c='k', zorder=1, marker='^')
+ax2_1.scatter(P[good], f[good], c='k')
+ax2_2.scatter(P[good], xi[good], c='k')
 
 
 line_P = np.logspace(-3, 2, 100)
-#line_f = 0.78
-#line_xi = 2/3
-#zeta = (line_xi*line_f/2)*np.sqrt(line_P/(1-line_f))
-#line_theory = np.sqrt(line_P * (1 - line_f)) * (np.sqrt(zeta**2 + 1) - zeta)
-#ax2.plot(line_P, line_theory, c='orange', label=r'theory ($f = {{{:.2f}}}, \xi = {{{:.2f}}}$)'.format(line_f, line_xi), zorder=0)
-#ax2.legend(frameon=True, fontsize=10, framealpha=0.6)
-#ax2_1.axhline(line_f, c='orange', lw=0.5, zorder=0)
-#ax2_2.axhline(line_xi, c='orange', lw=0.5, zorder=0)
 line_theory = 0.25 * line_P**(1/2)
 theory_plot = ax2.plot(line_P, line_theory, c='orange', label=r'$\propto \mathcal{P}_L^{1/2}$', zorder=0)
 ax2.legend(frameon=True, fontsize=10, framealpha=0.6)
@@ -135,13 +106,10 @@ for ax in [ax1, ax2]:
 
 for ax in [ax1_1, ax2_1]:
     ax.set_ylim(0, 1)
-#    ax.set_ylim(0.45, 0.95)
     ax.set_yticks((0, 0.25, 0.5, 0.75))
-#    ax.set_yticks((0.5, 0.7, 0.9))
 
 for ax in [ax1_2, ax2_2]:
     ax.set_ylim(0, 1)
-#    ax.set_ylim(0.4, 0.85)
     ax.set_yticks((0, 0.25, 0.5, 0.75))
 
 for ax in [ax1, ax1_1, ax1_2]:
@@ -164,9 +132,3 @@ ax2_2.set_xlabel(r'$\mathcal{P}_L$')
 
 plt.savefig('parameters_vs_p.png', dpi=300, bbox_inches='tight')
 plt.savefig('../manuscript/parameters_vs_p.pdf', dpi=300, bbox_inches='tight')
-
-#plt.close()
-#plt.figure()
-#good = (erf == 1)*(Re == 4e2)*(S ==1e3)
-#plt.scatter(theory_f[good], theory_f[good]/vel_rms[good]**2)
-#plt.show()
--- data/plot_results_vs_re.py
+++ data/plot_results_vs_re.py
@@ -5,120 +5,102 @@ import matplotlib.pyplot as plt
 plt.style.use('apj')
 import h5py
 
-import dedalus.public as de
-from scipy.interpolate import interp1d
-from scipy.optimize import brentq
-
 col_width = 3.25
 page_width = 6.5
 golden_ratio = 1.61803398875
 
-dirs = glob.glob('noslip_erf_Re_cut/erf*/')
-dirs += ['noslip_erf_P_cut/erf_step_3D_Re4e2_P4e0_zeta1e-3_S1e3_Lz2_Lcz1_Pr0.5_a2_Titer0_64x64x256/']
-dirs += glob.glob('stressfree_erf_Re_cut/erf*/')
-dirs.sort(key = lambda x: float(x.split('_Re')[-1].split('_')[0]))
+calculate_BLs = False
 
-#nz = 1024
-#Lz = 2
-#z_basis = de.Chebyshev('z', nz, interval=[0, Lz], dealias=1)
-#domain = de.Domain([z_basis], grid_dtype=np.float64)
-#ke_rhs = domain.new_field()
-#ke_flux = domain.new_field()
-#domain_z = domain.grid(-1)
+if calculate_BLs:
 
-cmap = "viridis_r"
-norm = mpl.colors.Normalize(vmin=np.log10(50), vmax=3.5)
-sm = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+    dirs = glob.glob('noslip_erf_Re_cut/erf*/')
+    dirs += ['noslip_erf_P_cut/erf_step_3D_Re4e2_P4e0_zeta1e-3_S1e3_Lz2_Lcz1_Pr0.5_a2_Titer0_64x64x256/']
+    dirs += glob.glob('stressfree_erf_Re_cut/erf*/')
+    dirs.sort(key = lambda x: float(x.split('_Re')[-1].split('_')[0]))
 
-data = []
-for i, d in enumerate(dirs):
-    S = float(d.split('_S')[-1].split('_')[0])
-    P = float(d.split('_Pr')[0].split('_P')[-1].split('_')[0])
-    Re = float(d.split('_Re')[-1].split('_')[0])
-    Lz = float(d.split('_Lz')[-1].split('_')[0])
-    with h5py.File('{:s}/data_top_cz.h5'.format(d), 'r') as f:
-        times = f['times'][()]
-        L_d01s = f['L_d01s'][()]
-        L_d05s = f['L_d05s'][()]
-        L_d09s = f['L_d09s'][()]
-        modern_f = f['modern_f'][()]
-        modern_xi = f['modern_xi'][()]
-        fconv_Ls = f['fconv_Ls'][()]
-        fconv_cz = f['fconv_cz'][()]
-        Ls = np.mean(f['Ls'][()])
-        tot_time = times[-1] - times[0]
-        time_window = np.min((500, tot_time-50))
-        time_window = np.min((500, tot_time-50))
-        good_times = times > (times[-1] - time_window)
-        mean_L_d01 = np.mean(L_d01s[good_times])
-        mean_L_d05 = np.mean(L_d05s[good_times])
-        mean_L_d09 = np.mean(L_d09s[good_times])
-        mean_f  = np.mean(modern_f[good_times])
-        std_f   = np.std(modern_f[good_times])/np.sqrt(len(modern_f[good_times]))
-        mean_xi = np.mean(modern_xi[good_times])
-        mean_fconv_Ls = np.mean(fconv_Ls[good_times])
-        mean_fconv_cz = np.mean(fconv_cz[good_times])
-    if 'stressfree' in d:
-        sf = True
-    else:
-        sf = False
-
-
-
-    with h5py.File('{:s}/avg_profs/averaged_avg_profs.h5'.format(d), 'r') as f:
-        if 'F_KE_p' not in f.keys():
-            pz_visc_bl = 0
-            bot_visc_bl = 0
+    data = []
+    for i, d in enumerate(dirs):
+        S = float(d.split('_S')[-1].split('_')[0])
+        P = float(d.split('_Pr')[0].split('_P')[-1].split('_')[0])
+        Re = float(d.split('_Re')[-1].split('_')[0])
+        Lz = float(d.split('_Lz')[-1].split('_')[0])
+        if 'stressfree' in d:
+            sf = 1
         else:
+            sf = 0
+        with h5py.File('{:s}/avg_profs/averaged_avg_profs.h5'.format(d), 'r') as f:
             enstrophy_profiles = f['enstrophy'][()]
-            vel_rms_profiles = f['vel_rms'][()]
             fconv_profiles = f['F_conv'][()]
             f_ke_profiles = f['F_KE_p'][()]
-            grad_profiles = -f['T_z'][()]
-            grad_ad_profiles = -f['T_ad_z'][()]
-            grad_rad_profiles = -f['T_rad_z'][()]
             z = f['z'][()]
 
             dissipation = enstrophy_profiles[-2,:]/Re
-#            F_KE = fconv_profiles[-2,:] - dissipation #- np.gradient(f_ke_profiles[-2,:], z)
             F_visc = fconv_profiles[-2,:] - dissipation - np.gradient(f_ke_profiles[-2,:], z)
-
             if not sf:
-                bot_visc_bl = z[(np.argmax(F_visc[z < 1]))]
-                visc_bl_upper_bot = z[(z > 1.2)*(z < 1.8)][F_visc[(z > 1.2)*(z < 1.8)] < np.min(F_visc[(z > 1.2)*(z < 1.8)])*0.2][0]
-                visc_bl_upper_top = z[(z > 1.2)*(z < 1.8)][F_visc[(z > 1.2)*(z < 1.8)] < np.min(F_visc[(z > 1.2)*(z < 1.8)])*0.2][-1]
+                bot_visc_bl = 2*z[(np.argmax(F_visc[z < 1]))]
             else:
-                bot_visc_bl = z[(np.argmin(F_visc[z < 1]))]
-                visc_bl_upper_bot = z[z > 1][F_visc[z > 1] < np.min(F_visc[z > 1])*0.25][0]
-                visc_bl_upper_top = z[z > 1][F_visc[z > 1] < np.min(F_visc[z > 1])*0.25][-1]
-#            plt.plot(z, enstrophy_profiles[-2, :])
-#            plt.axvline(bot_visc_bl)
-#            plt.ylabel(sf)
-#            plt.xlabel(Re)
-#            plt.show()
+                bot_visc_bl = 2*z[(np.argmin(F_visc[z < 1]))]
 
-            pz_visc_bl = visc_bl_upper_top - visc_bl_upper_bot
+        data.append((S, P, Re, sf, bot_visc_bl))
 
+    outf = open('erf_viscous_boundaries.csv', 'w')
+    title = '      S,       P,       R, SF,    ell_nu\n'
+    outf.write(title)
+    for d in data:
+        string = '{:.1e}, {:.1e}, {:.1e}, {:2}, {:.3e}\n'.format(*tuple(d))
+        outf.write(string)
+    outf.close()
+bl_data = np.genfromtxt('erf_viscous_boundaries.csv', skip_header=1, delimiter=',')
+boundary_R   = bl_data[:,2]
+boundary_sf  = bl_data[:,3]
+boundary_ell = bl_data[:,4]
 
-    data.append((S, P, Re, Ls, Lz, sf, mean_L_d01, mean_L_d05, mean_L_d09, mean_f, mean_xi, mean_fconv_Ls, mean_fconv_cz, bot_visc_bl, pz_visc_bl, std_f))
-data = np.array(data)
-S = data[:,0]
-P = data[:,1]
-Re = data[:,2]
-Ls = data[:,3]
-Lz = data[:,4]
-sf = data[:,5]
+        
 
-L_d01 = data[:,6]
-L_d05 = data[:,7]
-L_d09 = data[:,8]
-theory_f  = data[:,9]
-theory_xi = data[:,10]
-fconv_Ls = data[:,11]
-fconv_cz = data[:,12]
-bot_bl = 2*data[:,13]
-pz_bl  = data[:,14]
-std_f  = data[:,15]
+data = np.genfromtxt('scalar_table_data.csv', skip_header=1, delimiter=',', dtype=np.unicode_)
+S  = np.array(data[:,0], dtype=np.float64)
+P  = np.array(data[:,1], dtype=np.float64)
+Re = np.array(data[:,2], dtype=np.float64)
+Lz = np.array(data[:,3], dtype=np.float64)
+erf = data[:,5]
+sf  = data[:,6]
+ae  = data[:,7]
+d01 = np.array(data[:,10], dtype=np.float64)
+d05 = np.array(data[:,11], dtype=np.float64)
+d09 = np.array(data[:,12], dtype=np.float64)
+f   = np.array(data[:,13], dtype=np.float64)
+xi  = np.array(data[:,14], dtype=np.float64)
+Ls = np.array(data[:,15], dtype=np.float64)
+
+tmp = np.zeros_like(erf, dtype=bool)
+for i in range(erf.shape[0]):
+    if 'False' in erf[i]:
+        tmp[i] = 0
+    elif 'True' in erf[i]:
+        tmp[i] = 1
+erf = np.copy(tmp)
+
+tmp = np.zeros_like(sf, dtype=bool)
+for i in range(sf.shape[0]):
+    if 'False' in sf[i]:
+        tmp[i] = 0
+    elif 'True' in sf[i]:
+        tmp[i] = 1
+sf = np.copy(tmp)
+
+tmp = np.zeros_like(ae, dtype=bool)
+for i in range(ae.shape[0]):
+    if 'False' in ae[i]:
+        tmp[i] = 0
+    elif 'True' in ae[i]:
+        tmp[i] = 1
+ae = np.copy(tmp)
+
+bot_bl = np.zeros_like(f)
+for i in range(S.shape[0]):
+    if S[i] == 1e3 and P[i] == 4:
+        bot_bl[i] = boundary_ell[(boundary_R == Re[i])*(boundary_sf == sf[i])]
+
 
 fig = plt.figure(figsize=(page_width, 2*page_width/(3*golden_ratio)))
 
@@ -129,14 +111,15 @@ ax4 = fig.add_axes([0.00, 0.00, 0.25, 0.40])
 ax5 = fig.add_axes([0.35, 0.00, 0.25, 0.40])
 ax6 = fig.add_axes([0.70, 0.00, 0.25, 0.40])
 
-good = sf == 1
-ax1.scatter(Re[good], (L_d09 - Ls)[good], facecolor=(1,1,1,0.5), color='k', marker='v')
-ax1.scatter(Re[good], (L_d05 - Ls)[good], facecolor=(1,1,1,0.5), color='k', marker='o', label='SF')
-ax1.scatter(Re[good], (L_d01 - Ls)[good], facecolor=(1,1,1,0.5), color='k', marker='^')
-good = sf == 0
-ax1.scatter(Re[good], (L_d09 - Ls)[good], color='k', marker='v', alpha=0.8, s=15)
-ax1.scatter(Re[good], (L_d05 - Ls)[good], color='k', marker='o', label='NS', alpha=0.8, s=15)
-ax1.scatter(Re[good], (L_d01 - Ls)[good], color='k', marker='^', alpha=0.8, s=15)
+good_sf = (erf == 1) * (ae == 1) * (sf == 1) * (S == 1e3) * (P == 4) 
+good_ns = (erf == 1) * (ae == 1) * (sf == 0) * (S == 1e3) * (P == 4) 
+
+ax1.scatter(Re[good_sf], d09[good_sf], facecolor=(1,1,1,0.5), color='k', marker='v')
+ax1.scatter(Re[good_sf], d05[good_sf], facecolor=(1,1,1,0.5), color='k', marker='o', label='SF')
+ax1.scatter(Re[good_sf], d01[good_sf], facecolor=(1,1,1,0.5), color='k', marker='^')
+ax1.scatter(Re[good_ns], d09[good_ns], color='k', marker='v', alpha=0.8, s=15)
+ax1.scatter(Re[good_ns], d05[good_ns], color='k', marker='o', label='NS', alpha=0.8, s=15)
+ax1.scatter(Re[good_ns], d01[good_ns], color='k', marker='^', alpha=0.8, s=15)
 ax1.set_xscale('log')
 ax1.legend(frameon=True, fontsize=8, framealpha=0.6)
 ax1.set_xlabel(r'$\mathcal{R}$')
@@ -144,28 +127,25 @@ ax1.set_ylabel(r'$\delta_{0.5}$')
 ax1.set_xlim(2e1, 7e3)
 
 
-good = sf == 1
-ax2.scatter(Re[good], theory_f[good], facecolor=(1,1,1,0.5), color='k', marker='o')
-good = sf == 0
-ax2.scatter(Re[good], theory_f[good], color='k', marker='o', alpha=0.8, s=15)
+ax2.scatter(Re[good_sf], f[good_sf], facecolor=(1,1,1,0.5), color='k', marker='o')
+ax2.scatter(Re[good_ns], f[good_ns], color='k', marker='o', alpha=0.8, s=15)
 ax2.set_xscale('log')
 ax2.set_xlabel(r'$\mathcal{R}$')
 ax2.set_ylabel(r'$f$')
 ax2.set_xlim(2e1, 7e3)
 
-good = (sf == 1)
-ax3.scatter(Re[good], theory_xi[good], facecolor=(1,1,1,0.5), color='k', marker='o')
-good = sf == 0
-ax3.scatter(Re[good], theory_xi[good], color='k', marker='o', alpha=0.8, s=15)
+ax3.scatter(Re[good_sf], xi[good_sf], facecolor=(1,1,1,0.5), color='k', marker='o')
+ax3.scatter(Re[good_ns], xi[good_ns], color='k', marker='o', alpha=0.8, s=15)
 ax3.set_xscale('log')
 ax3.set_ylabel(r'$\xi$')
 ax3.set_xlabel(r'$\mathcal{R}$')
 ax3.set_xlim(2e1, 7e3)
 
-good = (sf == 1)*(Re >= 200)
-ax4.scatter(theory_f[good], (L_d05 - Ls)[good], facecolor=(1,1,1,0.5), color='k', marker='o')
-good = (sf == 0)*(Re >= 200)
-ax4.scatter(theory_f[good], (L_d05 - Ls)[good], color='k', marker='o', alpha=0.8, s=15)
+good_sf *= (Re >= 200)
+good_ns *= (Re >= 200)
+
+ax4.scatter(f[good_sf], d05[good_sf], facecolor=(1,1,1,0.5), color='k', marker='o')
+ax4.scatter(f[good_ns], d05[good_ns], color='k', marker='o', alpha=0.8, s=15)
 ax4.set_xlabel(r'$f$')
 ax4.set_ylabel(r'$\delta_{0.5}$')
 ax4.set_xlim(0.63, 0.75)
@@ -173,17 +153,15 @@ ax4.set_ylim(0.3, 0.55)
 
 
 
-good = (sf == 1)*(Re >= 200)
-ax5.errorbar(bot_bl[good], theory_f[good], yerr=std_f[good], lw=0, markerfacecolor=(1,1,1,1), color='k', marker='o')
-good = (sf == 0)*(Re >= 200)
-ax5.scatter(bot_bl[good], theory_f[good], color='k', marker='o', alpha=0.8, s=15)
+ax5.scatter(bot_bl[good_sf], f[good_sf], facecolor=(1,1,1,0.5), color='k', marker='o')
+ax5.scatter(bot_bl[good_ns], f[good_ns], color='k', marker='o', alpha=0.8, s=15)
 #ax5.errorbar(bot_bl[good], theory_f[good], yerr=std_f[good], lw=0, color='k', marker='o', alpha=0.8, markersize=5)
 ax5.set_xlim(0, 0.24)
 ax5.set_xlabel(r'$\ell_\nu$')
 ax5.set_ylabel(r'$f$')
 
 bl = np.linspace(-0.05, 0.4, 100)
-f_bl = 0.755*(1 - bl/Ls[good][0])
+f_bl = 0.755*(1 - bl/Ls[good_sf][0])
 ax5.plot(bl, f_bl, c='orange', label=r'$0.755(1 - \ell_\nu/L_s)$')
 ax5.set_ylim(0.6, 0.8)
 ax5.legend(frameon=True, fontsize=8, framealpha=0.6)
@@ -193,16 +171,13 @@ delta_func = lambda f_val: 0.9 * 4 * (1 - f_val) / ( 1 + xi * f_val * 4)
 delta = delta_func(f_bl)
 ax4.plot(f_bl, delta, c='orange', label=r'$0.9 \cdot \rm{Eqn}.~17$')
 ax4.legend(frameon=True, fontsize=8, framealpha=0.6)
-
 ax1.axhline(delta_func(0.755), c='orange')
 
 
 R = np.logspace(1, 4, 100)
 ax6.plot(R, 8*R**(-2/3), c='orange', label=r'$ 8 \mathcal{R}^{-2/3}$')
-good = (sf == 1)*(Re >= 200)
-ax6.scatter(Re[good], bot_bl[good], facecolor=(1,1,1,0.5), color='k', marker='o')
-good = (sf == 0)*(Re >= 200)
-ax6.scatter(Re[good], bot_bl[good], color='k', marker='o', alpha=0.8, s=15)
+ax6.scatter(Re[good_sf], bot_bl[good_sf], facecolor=(1,1,1,0.5), color='k', marker='o')
+ax6.scatter(Re[good_ns], bot_bl[good_ns], color='k', marker='o', alpha=0.8, s=15)
 ax6.set_xscale('log')
 ax6.set_yscale('log')
 ax6.set_xlabel(r'$\mathcal{R}$')
@@ -210,13 +185,6 @@ ax6.set_ylabel(r'$\ell_\nu$')
 ax6.legend(frameon=True, fontsize=8, framealpha=0.6)
 ax6.set_xlim(1.5e2, 7e3)
 ax6.set_ylim(1.4e-2, 6e-1)
-
-#for ax in [ax1, ax2, ax3]:
-#    ax.xaxis.set_ticks_position('top')
-#    ax.xaxis.set_label_position('top')
-
-print(Ls, bot_bl[(sf == 1)*(Re >= 200)], Re[(sf == 1)*(Re >= 200)], delta_func(0.734), delta_func(0.755))
-
 
 plt.savefig('parameters_vs_re.png', dpi=300, bbox_inches='tight')
 plt.savefig('../manuscript/parameters_vs_re.pdf', dpi=300, bbox_inches='tight')
